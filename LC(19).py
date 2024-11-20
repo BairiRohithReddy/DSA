1b4945939adccf236cd34836b9cf8d55ce8e0011
Rohith Reddy Bairi <rohithbairi16@gmail.com>
2024-11-20 17:29:15 -0600

    modified LC(19) and LC(21) slightly

--- LC(19).py
+++ LC(19).py
@@ -29,11 +29,12 @@ The number of nodes in the list is sz.
  
 '''
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#Definition for singly-linked list.
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
     # def length(self):
     #     currentNode = head
